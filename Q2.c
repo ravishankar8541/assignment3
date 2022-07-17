a2baf66657befa0df5f2c28cd87aa90337c80905
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-17 23:08:15 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,11 @@
+//2. Write a program to check whether a given number is divisible by 5 or not
+#include"stdio.h"
+int main()
+{
+    int n=6;
+    if(n%5==0)
+    printf("divisible by 5");
+    else
+    printf("not divisible by 5");
+    return 0;
+}
