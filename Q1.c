824d4b29505831eeb4e68ab92465804e317fab23
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-17 23:07:59 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,13 @@
+//Write a program to check whether a given number is positive or non-positive.
+#include"stdio.h"
+int main()
+{
+    int n=9;
+    if(n>0){
+    printf("positive");
+    }
+    else if(n<0){
+    printf("non-positive");
+    }
+    return 0;
+}
