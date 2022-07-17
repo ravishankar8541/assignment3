841dfec36bb1bada379ec9306205861cdb0c571a
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-17 23:08:36 +0530

    Add files via upload

---
+++ Q3.c
@@ -0,0 +1,12 @@
+//Write a program to check whether a given number is an even number or an odd
+//number.
+#include"stdio.h"
+int main()
+{
+    int num=8;
+    if(num%2==0)
+    printf("even number ");
+    else
+    printf("odd number ");
+    return 0;
+}
